c91aeba10a9d95de4985d9898ba90174d2b6c4ff
Kiran Europa <kceuropa1@gmail.com>
2025-02-22 10:58:41 +0200

    Add files via upload

---
+++ Product.java
@@ -0,0 +1,22 @@
+public class Product {
+
+    public static void main(String[] args) {
+
+   // THIS IS MY FIRST JAVA PROGRAM
+   /* THIS
+   IS
+   A
+   MULTI LINE
+   COMMENT
+    */
+
+
+
+
+    System.out.println("I like Manchester United");
+    System.out.println("I like FC Barcelona");
+    System.out.println("I like AC Milan");
+
+
+    }
+}
